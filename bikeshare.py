f0d19ea167c22a6b3eddcfd0c808bab9a84ab0d0
User <you@example.com>
2022-12-02 17:08:28 +0300

    feat : Add new file bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -9,6 +9,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 def get_filters():
     """
+    add by rama
     Asks user to specify a city, month, and day to analyze.
     Returns:
         (str) city - name of the city to analyze
